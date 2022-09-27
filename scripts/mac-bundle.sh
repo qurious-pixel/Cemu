d8cd489e7aeb1428e5e2b541a759fc42ee8c4c4b
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-09-27 15:40:01 -0700

    Update mac-bundle.sh

--- scripts/mac-bundle.sh
+++ scripts/mac-bundle.sh
@@ -8,17 +8,17 @@ APPBUNDLERESOURCES=$APPBUNDLECONTENTS/Resources
 APPBUNDLEICON=$APPBUNDLECONTENTS/Resources
 
 mkdir dist/mac/$APPNAME.iconset
-sips -z 16 16     dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_16x16.png
-sips -z 32 32     dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_16x16@2x.png
-sips -z 32 32     dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_32x32.png
-sips -z 64 64     dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_32x32@2x.png
-sips -z 128 128   dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_128x128.png
-sips -z 256 256   dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_128x128@2x.png
-sips -z 256 256   dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_256x256.png
-sips -z 512 512   dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_256x256@2x.png
-sips -z 512 512   dist/mac/$APPNAMEIcon.png --out macosx/$(APPNAME).iconset/icon_512x512.png
-cp dist/mac/$APPNAMEIcon.png dist/mac/$APPNAME.iconset/icon_512x512@2x.png
-iconutil -c icns -o macosx/$APPNAME.icns macosx/$APPNAME.iconset
+sips -z 16 16     dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_16x16.png
+sips -z 32 32     dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_16x16@2x.png
+sips -z 32 32     dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_32x32.png
+sips -z 64 64     dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_32x32@2x.png
+sips -z 128 128   dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_128x128.png
+sips -z 256 256   dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_128x128@2x.png
+sips -z 256 256   dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_256x256.png
+sips -z 512 512   dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_256x256@2x.png
+sips -z 512 512   dist/mac/$APPNAME.png --out dist/mac/$APPNAME.iconset/icon_512x512.png
+cp dist/mac/$APPNAME.png dist/mac/$APPNAME.iconset/icon_512x512@2x.png
+iconutil -c icns -o macosx/$APPNAME.icns dist/mac/$APPNAME.iconset
 	
 mkdir $APPBUNDLE
 mkdir $APPBUNDLE/Contents
