de371a86511ef73e236dda64bb200f4bbf1c05f8
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-10-27 21:33:25 -0700

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 
-yum install -y ccache
-
-cmake -S . -B build -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=/usr/lib64/ccache -DCMAKE_CXX_COMPILER=/usr/lib64/ccache -DENABLE_VCPKG=OFF -DPORTABLE=OFF -G Ninja
+cmake -S . -B build -DCMAKE_BUILD_TYPE=release -DENABLE_VCPKG=OFF -DPORTABLE=OFF -G Ninja
 cmake --build build
 
 mv bin/Cemu_release bin/Cemu 
