6a3b0bcb8d7dd3c0b8f8b848eb742cfab44221fd
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-10-28 05:01:16 -0700

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,7 +2,3 @@
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=release -DENABLE_VCPKG=OFF -DPORTABLE=OFF -G Ninja
 cmake --build build
-
-mv bin/Cemu_release bin/Cemu 
-
-chmod +x dist/linux/appimage.sh && dist/linux/appimage.sh
