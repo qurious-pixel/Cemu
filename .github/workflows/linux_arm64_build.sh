3204b7e6c3ed8965fa886784b9c370909a75e58c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-06-29 09:22:06 -0700

    Create linux_arm64_build.sh

---
+++ .github/workflows/linux_arm64_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -ex
+
+cd cemu || exit 1
+
+git config --global --add safe.directory '*'
+
+
+sudo apt update -qq
+sudo apt install -y clang cmake freeglut3-dev libgcrypt20-dev libglm-dev libgtk-3-dev libpulse-dev libsecret-1-dev libsystemd-dev libudev-dev nasm ninja-build libbluetooth-dev
+
+
+bash /cemu/dependencies/vcpkg/bootstrap-vcpkg.sh
+
+cmake -S . -B build  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja
+cmake --build build
+
+# mv bin/Cemu_release bin/Cemu
+
