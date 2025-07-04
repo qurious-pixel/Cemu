2c220a7cd51a4d39128548b74e9941d968f69eaa
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-07-03 21:41:20 -0700

    Update ih264_intra_pred_chroma_av8.s

--- dependencies/ih264d/common/armv8/ih264_intra_pred_chroma_av8.s
+++ dependencies/ih264d/common/armv8/ih264_intra_pred_chroma_av8.s
@@ -435,10 +435,12 @@ ih264_intra_pred_chroma_8x8_mode_plane_av8:
     ldr       x12, [x12, _ih264_gai1_intrapred_chroma_plane_coeffs1@GOTPAGEOFF]
 # .endif
 #endif
-.ifndef __APPLE__
+#ifndef __APPLE__
+# .ifndef __APPLE__
     adrp      x12, :got:ih264_gai1_intrapred_chroma_plane_coeffs1
     ldr       x12, [x12, #:got_lo12:ih264_gai1_intrapred_chroma_plane_coeffs1]
-.endif
+# .endif
+#endif
     usubl     v10.8h, v5.8b, v1.8b
     ld1       {v8.8b, v9.8b}, [x12]     // Load multiplication factors 1 to 8 into D3
     mov       v8.d[1], v9.d[0]
@@ -492,13 +494,18 @@ ih264_intra_pred_chroma_8x8_mode_plane_av8:
     zip1      v1.8h, v0.8h, v2.8h
     zip2      v2.8h, v0.8h, v2.8h
     mov       v0.16b, v1.16b
-.ifdef __APPLE__
+#ifdef __APPLE__
+# .ifdef __APPLE__
     adrp      x12, _ih264_gai1_intrapred_chroma_plane_coeffs2@GOTPAGE
     ldr       x12, [x12, _ih264_gai1_intrapred_chroma_plane_coeffs2@GOTPAGEOFF]
-.else
+# .endif
+#endif
+#ifndef __APPLE__
+# .ifndef __APPLE__
     adrp      x12, :got:ih264_gai1_intrapred_chroma_plane_coeffs1
     ldr       x12, [x12, #:got_lo12:ih264_gai1_intrapred_chroma_plane_coeffs1]
-.endif
+# .endif
+#endif
     ld1       {v8.2s, v9.2s}, [x12]
     mov       v8.d[1], v9.d[0]
     mov       v10.16b, v8.16b
