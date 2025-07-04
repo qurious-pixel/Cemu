0e78aaa086df426016396085fc5bbd15489b4c93
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-07-03 17:02:26 -0700

    Update linux_arm64_build.sh

--- .github/workflows/linux_arm64_build.sh
+++ .github/workflows/linux_arm64_build.sh
@@ -6,7 +6,7 @@ git config --global --add safe.directory '*'
 
 
 apt update -qq
-apt install -y autoconf clang cmake freeglut3-dev git libbluetooth-dev libgcrypt20-dev libglm-dev libgtk-3-dev libltdl-dev libpulse-dev libsecret-1-dev libsystemd-dev libudev-dev nasm ninja-build zip
+apt install -y autoconf clang-15 cmake freeglut3-dev git libbluetooth-dev libgcrypt20-dev libglm-dev libgtk-3-dev libltdl-dev libpulse-dev libsecret-1-dev libsystemd-dev libudev-dev nasm ninja-build zip
 
 cd /cemu/dependencies/vcpkg
 git remote update
@@ -20,7 +20,7 @@ cd /cemu
 #cd /cemu/dependencies/ih264d
 ##
 
-cmake -S . -B build  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DCMAKE_VERBOSE_MAKEFILE=ON
+cmake -S . -B build  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja
 cmake --build build
 
 # mv bin/Cemu_release bin/Cemu
