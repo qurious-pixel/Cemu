1378c5d28fd795d196d2df48516f5fbb624c34e2
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-07-03 21:16:15 -0700

    build ih264d only

--- .github/workflows/linux_arm64_build.sh
+++ .github/workflows/linux_arm64_build.sh
@@ -5,19 +5,19 @@ cd cemu || exit 1
 git config --global --add safe.directory '*'
 
 
-apt update -qq
-apt install -y autoconf clang-15 cmake freeglut3-dev git libbluetooth-dev libgcrypt20-dev libglm-dev libgtk-3-dev libltdl-dev libpulse-dev libsecret-1-dev libsystemd-dev libudev-dev nasm ninja-build zip
+#apt update -qq
+#apt install -y autoconf clang-15 cmake freeglut3-dev git libbluetooth-dev libgcrypt20-dev libglm-dev libgtk-3-dev libltdl-dev libpulse-dev libsecret-1-dev libsystemd-dev libudev-dev nasm ninja-build zip
 
-cd /cemu/dependencies/vcpkg
-git remote update
-git pull origin master
-./bootstrap-vcpkg.sh
-cd /cemu
+#cd /cemu/dependencies/vcpkg
+#git remote update
+#git pull origin master
+#./bootstrap-vcpkg.sh
+#cd /cemu
 
 ## test build
-#apt update -qq
-#apt install -y clang
-#cd /cemu/dependencies/ih264d
+apt update -qq
+apt install -y clang-15
+cd /cemu/dependencies/ih264d
 ##
 
 cmake -S . -B build  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang-15 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja
