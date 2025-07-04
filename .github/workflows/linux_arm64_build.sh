8b3353a66116b1205e2e30b307300f7c2c10055c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-07-03 17:16:52 -0700

    Update linux_arm64_build.sh

--- .github/workflows/linux_arm64_build.sh
+++ .github/workflows/linux_arm64_build.sh
@@ -20,7 +20,7 @@ cd /cemu
 #cd /cemu/dependencies/ih264d
 ##
 
-cmake -S . -B build  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja
+cmake -S . -B build  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang-15 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja
 cmake --build build
 
 # mv bin/Cemu_release bin/Cemu
