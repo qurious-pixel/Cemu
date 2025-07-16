cc3e5a9ae5920166b7313cab48962c04877d7fb1
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-07-16 12:57:24 -0700

    Update freebsd_build.sh

--- .github/workflows/freebsd_build.sh
+++ .github/workflows/freebsd_build.sh
@@ -2,7 +2,7 @@
 
 sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
 export ASSUME_ALWAYS_YES=true
-pkg info # debug
+pkg info # debug #
 pkg install llvm16
 pkg install git ccache cmake ninja ffmpeg
 pkg install pkgconf alsa-lib pulseaudio sdl3 evdev-proto vulkan-headers vulkan-loader opencv
