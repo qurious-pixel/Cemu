f5da4b05b8a0f1a63d0013fd8fb5e13fa8ecab55
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-07-21 08:48:16 -0700

    Update freebsd_build.sh

--- .github/workflows/freebsd_build.sh
+++ .github/workflows/freebsd_build.sh
@@ -3,9 +3,7 @@
 sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
 export ASSUME_ALWAYS_YES=true
 pkg info # debug #
-pkg install llvm16
-pkg install git ccache cmake ninja ffmpeg
-pkg install pkgconf alsa-lib pulseaudio sdl3 evdev-proto vulkan-headers vulkan-loader opencv
+pkg install -y boost-libs cmake-core curl glslang gtk3 ninja png pkgconf rapidjson wayland wayland-protocols wx32-gtk3 xorg zstd
 
-cmake -B build -G Ninja
+cmake -B build -DCMAKE_BUILD_TYPE=release -DENABLE_BLUEZ=OFF -DENABLE_DISCORD_RPC=OFF -DENABLE_FERAL_GAMEMODE=OFF -DENABLE_HIDAPI=OFF -DENABLE_VCPKG=OFF -G Ninja
 cmake --build build
