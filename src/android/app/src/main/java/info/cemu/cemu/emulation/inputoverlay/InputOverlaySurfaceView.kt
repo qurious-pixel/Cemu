e3ea20f9a811b0bdff4a99c29ecb8b5f408fc5ea
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-12-12 10:13:29 -0800

    Update InputOverlaySurfaceView.kt

--- src/android/app/src/main/java/info/cemu/cemu/emulation/inputoverlay/InputOverlaySurfaceView.kt
+++ src/android/app/src/main/java/info/cemu/cemu/emulation/inputoverlay/InputOverlaySurfaceView.kt
@@ -1,8 +1,11 @@
 package info.cemu.cemu.emulation.inputoverlay
 
 import android.content.Context
+import android.content.res.Configuration
+import android.content.pm.PackageManager
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
