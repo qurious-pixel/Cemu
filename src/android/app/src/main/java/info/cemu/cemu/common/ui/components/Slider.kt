2c0948fe7dc82a518c5ce355a2abcdce92838189
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-12-16 10:33:51 -0800

    Update Slider.kt

--- src/android/app/src/main/java/info/cemu/cemu/common/ui/components/Slider.kt
+++ src/android/app/src/main/java/info/cemu/cemu/common/ui/components/Slider.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.progressBarRangeInfo
 import androidx.compose.ui.semantics.setProgress
 import androidx.compose.ui.semantics.ProgressBarRangeInfo
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
