39d9a8721526ec626170a07d593368e180d6a2c8
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-12-17 11:10:20 -0800

    Update build.gradle.kts

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -218,6 +218,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     testImplementation(libs.archunit.junit4)
     androidTestImplementation(libs.androidx.junit)
