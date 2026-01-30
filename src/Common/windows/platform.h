36a3a73d2d45372f0151a57e7bb810a67d918095
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2026-01-30 11:37:05 -0800

    Update platform.h

--- src/Common/windows/platform.h
+++ src/Common/windows/platform.h
@@ -3,20 +3,10 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
-
-#include <winsock2.h>
-#include <ws2tcpip.h>
-
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#ifdef __MINGW32__
-extern "C++" {
-    #include <Windows.h>
-}
-#else
-    #include <Windows.h>
-#endif
+#include <Windows.h>
 
 #define AF_BLUETOOTH AF_BTH
 #define BTPROTO_RFCOMM BT_PORT_ANY
