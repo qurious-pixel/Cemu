2876e458694ace2288e8e12c67cca795b76c0f3c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2026-05-07 09:07:44 -0700

    Update SystemException.h

--- src/util/helpers/SystemException.h
+++ src/util/helpers/SystemException.h
@@ -1,20 +1,26 @@
 #pragma once
 
+#include <stdexcept>
+#include <cstdint>
 #include "util/helpers/helpers.h"
+
+#ifndef _WIN32
+using DWORD = uint32_t;
+#endif
 
 class SystemException : public std::runtime_error
 {
 public:
 	SystemException()
-		: std::runtime_error(GetSystemErrorMessage().c_str()), m_error_code(GetExceptionError())
+		: std::runtime_error(GetSystemErrorMessage()), m_error_code(GetExceptionError())
 	{}
 
 	SystemException(const std::exception& ex)
-		: std::runtime_error(GetSystemErrorMessage(ex).c_str()), m_error_code(GetExceptionError())
+		: std::runtime_error(GetSystemErrorMessage(ex)), m_error_code(GetExceptionError())
 	{}
 	
 	SystemException(const std::error_code& ec)
-		: std::runtime_error(GetSystemErrorMessage(ec).c_str()), m_error_code(GetExceptionError())
+		: std::runtime_error(GetSystemErrorMessage(ec)), m_error_code(GetExceptionError())
 	{}
 
 	[[nodiscard]] DWORD GetErrorCode() const { return m_error_code; }
