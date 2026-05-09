9d2971426778dfd1fbbc23dc467b1e6103fe7e86
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2026-05-09 14:28:26 -0700

    Update PPCTimer.cpp

--- src/Cafe/HW/Espresso/PPCTimer.cpp
+++ src/Cafe/HW/Espresso/PPCTimer.cpp
@@ -1,19 +1,10 @@
 #if defined(_M_ARM64)
 #include <intrin.h>
-
+#define _mm_mfence() __dmb(_ARM64_BARRIER_ISH)
+#define __rdtsc() _ReadStatusReg(ARM64_CNTVCT_EL0)
 extern "C" uint64_t _umul128(uint64_t a, uint64_t b, uint64_t* high) {
     *high = __umulh(a, b);
     return a * b;
-}
-
-extern "C" uint64_t _udiv128(uint64_t high, uint64_t low, uint64_t divisor, uint64_t* remainder) {
-    if (high == 0) {
-        *remainder = low % divisor;
-        return low / divisor;
-    }
-    unsigned __int128 dividend = ((unsigned __int128)high << 64) | low;
-    *remainder = (uint64_t)(dividend % divisor);
-    return (uint64_t)(dividend / divisor);
 }
 #endif
 
