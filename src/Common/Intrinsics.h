2bbb71580dad669d58dba36182627e0cfd4e583d
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2026-05-10 12:17:06 -0700

    Update Intrinsics.h

--- src/Common/Intrinsics.h
+++ src/Common/Intrinsics.h
@@ -4,7 +4,27 @@
 #if defined(_MSC_VER)
     #include <intrin.h>
     #if defined(_M_ARM64)
+        #include <arm64intrin.h>
         extern "C" uint64_t UnsignedDivision128(uint64_t high, uint64_t low, uint64_t divisor, uint64_t* remainder);
+        #define BARRIER_FENCE() __dmb(_ARM64_BARRIER_ISH)
+        #define READ_TSC()      _ReadStatusReg(ARM64_CNTVCT_EL0)
+    #else
+        #include <immintrin.h>
+        #define BARRIER_FENCE() _mm_mfence()
+        #define READ_TSC()      __rdtsc()
+    #endif
+#else 
+    #if defined(__aarch64__)
+        #define BARRIER_FENCE() __asm__ __volatile__ ("dmb ish" : : : "memory")
+        static inline uint64_t READ_TSC() {
+            uint64_t val;
+            __asm__ __volatile__("mrs %0, cntvct_el0" : "=r" (val));
+            return val;
+        }
+    #else
+        #include <x86intrin.h>
+        #define BARRIER_FENCE() __asm__ __volatile__ ("mfence" : : : "memory")
+        #define READ_TSC()      __rdtsc()
     #endif
 #endif
 
@@ -31,7 +51,6 @@ static inline uint64_t Divide128by64(uint64_t high, uint64_t low, uint64_t divis
 #elif defined(_MSC_VER) && defined(_M_X64)
     return _udiv128(high, low, divisor, remainder);
 #elif defined(_MSC_VER) && defined(_M_ARM64)
-    // Now just call it here
     return UnsignedDivision128(high, low, divisor, remainder);
 #else
     #error "128-bit division not supported on this platform"
