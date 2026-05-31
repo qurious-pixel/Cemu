2e2613d33a8546fe715103ec9fd1a06fdbcc6831
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2026-05-31 12:10:14 -0700

    Update InputDeviceExtensions.kt

--- src/android/app/src/main/java/info/cemu/cemu/common/android/inputdevice/InputDeviceExtensions.kt
+++ src/android/app/src/main/java/info/cemu/cemu/common/android/inputdevice/InputDeviceExtensions.kt
@@ -3,9 +3,16 @@ package info.cemu.cemu.common.android.inputdevice
 import android.view.InputDevice
 
 fun InputDevice.isGameController(): Boolean {
-    return !isVirtual && (
-            sources and InputDevice.SOURCE_GAMEPAD == InputDevice.SOURCE_GAMEPAD
-                    || sources and InputDevice.SOURCE_JOYSTICK == InputDevice.SOURCE_JOYSTICK
-                    || sources and InputDevice.SOURCE_DPAD == InputDevice.SOURCE_DPAD
-            )
+    if (isVirtual) {
+        return false
+    }
+
+    val isGamepad = (sources and InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD
+            || (sources and InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK
+            || (sources and InputDevice.SOURCE_DPAD) == InputDevice.SOURCE_DPAD
+
+    val isPhysicalKeyboard = (sources and InputDevice.SOURCE_KEYBOARD) == InputDevice.SOURCE_KEYBOARD 
+            && keyboardType == InputDevice.KEYBOARD_TYPE_ALPHABETIC
+
+    return isGamepad || isPhysicalKeyboard
 }
